db280d373b35d4740581927621d7d6557a5499c0
jiming <jimingwork@126.com>
2016-08-16 15:30:13 +0800

    init

---
+++ src/test/java/jm/learn/newtech/demo/TechDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package jm.learn.newtech.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
